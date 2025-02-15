968d2133909d0951861f7e7e546b5d08e3cef6c8
pierreOF <marcos.pierre@upe.br>
2025-02-15 15:23:30 -0300

    feat: arquivo main para execucao

--- src/main/groovy/org/example/Main.groovy
+++ src/main/groovy/org/example/Main.groovy
@@ -1,4 +1,19 @@
 package org.example
 
+import org.example.controller.MenuController
+import org.example.repository.CandidateRepository
+import org.example.repository.CompanyRepository
+import org.example.view.MenuView
+
 static void main(String[] args) {
+
+    CandidateRepository candidateRepository = new CandidateRepository()
+    CompanyRepository companyRepository = new CompanyRepository()
+    MenuView menuView = new MenuView()
+
+    MenuController menuController = new MenuController(candidateRepository,companyRepository,menuView)
+
+
+    menuController.start()
+
 }
