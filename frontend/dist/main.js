3c03dc2c488042626b0eec7dc7846591cb45e806
pierreOF <marcos.pierre@upe.br>
2025-03-11 19:02:22 -0300

    Build front

---
+++ frontend/dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+document.addEventListener("DOMContentLoaded", () => {
+    const app = document.getElementById("app");
+    if (app) {
+        app.innerHTML = "<h2>Frontend TypeScript Puro</h2>";
+    }
+});
---
+++ frontend/src/main.ts
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const app = document.getElementById("app");
+    if (app) {
+        app.innerHTML = "<h2>Frontend TypeScript Puro</h2>";
+    }
+});
